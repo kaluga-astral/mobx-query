9ea66e1446f1c8ab6f3cf090ee6c8ecbebc61373
ofigelov <konungivan@gmail.com>
2023-06-07 15:06:01 +0500

    chore: Upd configs

--- pack/src/modifyPackageJSON/modifyPackageJSON.js
+++ pack/src/modifyPackageJSON/modifyPackageJSON.js
@@ -66,10 +66,10 @@ const modifyPackageJSON = ({
     license: 'MIT',
     repository: {
       type: 'git',
-      url: 'git+https://github.com/kaluga-astral/frontend',
+      url: 'git+https://github.com/kaluga-astral/mobx-query',
     },
     bugs: {
-      url: 'https://github.com/kaluga-astral/frontend/issues',
+      url: 'https://github.com/kaluga-astral/mobx-query/issues',
     },
     keywords,
     sideEffects: false,
